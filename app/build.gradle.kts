d0c9a43c9b4fdbe81eafaab1f377c8b9e3e049b3
Pombo <github.5yeil@simplelogin.fr>
2026-04-25 10:20:07 -0700

    home/playlist ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.rama.tui"
         minSdk = 21
         targetSdk = 36
-        versionCode = 3
+        versionCode = 1
         versionName = "$currentYear.$versionCode"
     }
 
